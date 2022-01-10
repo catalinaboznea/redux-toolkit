fe058514f5812101f19b4d84f183abb50710f106
Catalina Boznea <catalina.boznea@tradeshift.com>
2022-01-10 18:12:26 +0100

    use RTK Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { useAppDispatch, useAppSelector } from "./app/hooks";
 import { incremented, amountAdded } from "./features/counter/counter-slice";
+import { useFetchBreedsQuery } from "./features/dogs/dogs-api-slice";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   const count = useAppSelector((state) => state.counter.value);
   const dispatch = useAppDispatch();
+
+  const { data = [], isFetching } = useFetchBreedsQuery();
 
   function handleClick() {
     // increment by 1
@@ -26,9 +29,29 @@ function App() {
             count is: {count}
           </button>
         </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
+
+        <div>
+          <p>Numbers of dogs fetched: {data.length}</p>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Picture</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((breed) => (
+                <tr key={breed.id}>
+                  <td>{breed.name}</td>
+                  <td>
+                    <img src={breed.image.url} alt={breed.name} height={250} />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+
         <p>
           <a
             className="App-link"
